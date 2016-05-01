5b5c64a5a9e2c375806afaec018e3da66d0b1f71
mikemajesty <mikemajesty2011@gmail.com>
2016-05-01 18:25:21 -0300

    Change the basic structure

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app  = express();
 
 mongoose.connect('mongodb://localhost:27017/banco');
 
+app.use('/app', express.static(__dirname  + '/app'));
+app.use('/node_modules',  express.static(__dirname  + '/node_modules'))
+
+
 app.get('/', (req,res) => {
   res.sendfile('index.html');
 });
